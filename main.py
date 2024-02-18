4b1914f09c0814f509a95b4596c96e6c2a2abe45
luis tapia <luis.tapia@utexas.edu>
2024-02-18 12:50:46 -0600

    df

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ token = os.getenv('TOKEN')
 intents = discord.Intents.default()
 intents.messages = True
 intents.guilds = True
+intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 @bot.event
 async def on_ready():
@@ -16,7 +17,7 @@ async def on_ready():
 
 # command: !playerStats
 @bot.command(name= 'playerStats', help='Displays stats for a specified player.')
-async def player_stats(ctx, playerName : str):
+async def player_stats(ctx):
     await ctx.send("testing playerStats")
 
 
