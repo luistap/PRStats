b989cc7c71aadcca73edf9781d9ae89466e5dd69
luis tapia <luis.tapia@utexas.edu>
2024-02-18 04:28:24 -0600

    mm

--- main.py
+++ main.py
@@ -13,12 +13,12 @@ async def onReady():
 
 # command: !playerStats
 @bot.command(name= 'playerStats', help='Displays stats for a specified player.')
-async def playerStats(ctx, playerName : str)
+async def playerStats(ctx, playerName : str):
     await ctx.send("testing playerStats")
 
 
 @bot.command(name='odds', help='Calculates and displays odds for a match.')
-async def matchOdds(ctx, matchInfo: str)
+async def matchOdds(ctx, matchInfo: str):
     await ctx.send("testing matchOdds")
 
 bot.run(token)
